289ad4c8fb9715e402c6436ffba39f8a8dcd9945
Roland Rabien <roland@rabien.com>
2019-06-13 17:02:37 -0700

    Added MidiPortMonitor

---
+++ Source/MainComponent.cpp
@@ -0,0 +1,96 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+  ==============================================================================
+*/
+
+#include "MainComponent.h"
+
+//==============================================================================
+MainComponent::MainComponent()
+{
+    setSize (600, 400);
+
+    startTimer (500);
+
+    addAndMakeVisible (text);
+    text.setReadOnly (true);
+    text.setMultiLine (true);
+}
+
+MainComponent::~MainComponent()
+{
+}
+
+//==============================================================================
+void MainComponent::paint (Graphics& g)
+{
+    g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
+}
+
+void MainComponent::resized()
+{
+    text.setBounds (getLocalBounds().reduced (8));
+}
+
+void MainComponent::timerCallback()
+{
+    auto inputs = MidiInput::getAvailableDevices();
+    auto outputs = MidiOutput::getAvailableDevices();
+
+    StringArray inNames, inIDs, outNames, outIDs;
+
+    for (auto input : inputs)
+    {
+        inNames.add (input.name);
+        inIDs.add (input.identifier);
+    }
+
+    for (auto output : outputs)
+    {
+        outNames.add (output.name);
+        outIDs.add (output.identifier);
+    }
+
+    inNames.sortNatural();
+    inIDs.sortNatural();
+
+    outNames.sortNatural();
+    outIDs.sortNatural();
+
+    if (inNames != lastInNames || inIDs != lastInIDs || outNames != lastOutNames || outIDs != lastOutIDs)
+    {
+        lastInNames  = inNames;
+        lastInIDs    = inIDs;
+        lastOutNames = outNames;
+        lastOutIDs   = outIDs;
+
+        dumpInfo();
+    }
+}
+
+void MainComponent::dumpInfo()
+{
+    auto inputs = MidiInput::getAvailableDevices();
+    auto outputs = MidiOutput::getAvailableDevices();
+
+    String str;
+
+    str += Time::getCurrentTime().toString (true, true) + "\n\n";
+    str += "Inputs: \n";
+
+    for (auto in : inputs)
+        str += in.name + " (" + in.identifier + ") \n";
+    str += "\n";
+
+    str += "Outputs: \n";
+
+    for (auto out : outputs)
+        str += out.name + " (" + out.identifier + ") \n";
+    str += "\n=======================================================\n\n";
+
+    text.moveCaretToEnd();
+    text.insertTextAtCaret (str);
+}
---
+++ Source/MainComponent.h
@@ -0,0 +1,40 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+//==============================================================================
+/*
+    This component lives inside our window, and this is where you should put all
+    your controls and content.
+*/
+class MainComponent : public Component,
+                      private Timer
+{
+public:
+    //==============================================================================
+    MainComponent();
+    ~MainComponent();
+
+    //==============================================================================
+    void paint (Graphics&) override;
+    void resized() override;
+    void timerCallback() override;
+
+private:
+    void dumpInfo();
+
+    //==============================================================================
+    TextEditor text;
+
+    StringArray lastInNames, lastInIDs, lastOutNames, lastOutIDs;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
+};
